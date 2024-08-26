645cd5778946925c99e33b6661055480738ef829
DuyKhanh2305 <phanduymanh2305@gmail.com>
2024-08-26 10:09:41 +0700

    Devided Components

---
+++ AgileWebApplication/Components/FeatureSection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgileWebApplication.Components
+{
+    public class FeatureSection : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ AgileWebApplication/Components/ProductList.cs
@@ -0,0 +1,18 @@
+﻿using AgileWebApplication.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgileWebApplication.Components
+{
+    public class ProductList : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductList(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View("Index",_context.Products.ToList());
+        }
+    }
+}
