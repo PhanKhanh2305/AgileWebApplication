bd2c6d11d8e985de2a5641a832936e11f4d0ae14
DuyKhanh2305 <phanduymanh2305@gmail.com>
2024-08-26 08:16:02 +0700

    Add project files.

---
+++ AgileWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgileWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
